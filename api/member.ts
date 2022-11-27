40911e549bf30d6203de97a607f8cd3b1dca2309
kitez <duswl339@ajou.ac.kr>
2022-11-27 22:21:47 +0900

    refactor: add Group page
    + 라우팅 수정

--- api/member.ts
+++ api/member.ts
@@ -1,50 +1,12 @@
 import axios from 'axios';
 import { SERVER_API } from '../config';
 
-export function getMyInfo() {
-  let result = null;
-  axios.get(
-    `${SERVER_API}/members/my/info`,
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    },
-  ).then((res) => {
-    result = res.data.data;
-    return result;
-  });
-}
-
-export function getMyClubs() {
-  let result = null;
-  axios.get(
-    `${SERVER_API}/members/my/clubs`,
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    },
-  ).then((res) => {
-    result = res.data.data;
-    return result;
-  });
-}
-
-// user가 가입된 clubs들에 대한 user의 role들 return
-export async function getMyClubsRoles() {
-  const result = [];
-  await axios.get(
-    `${SERVER_API}/members/my/clubs`,
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    },
-  ).then((res) => {
-    res.data.data.forEach((club) => {
-      result.push(club.clubRole);
-    });
-  });
-  return result;
+const API = axios.create({
+  baseURL: SERVER_API,
+});
+export async function getMyClubs() {
+  API.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  return API.get('/members/my/clubs')
+    .then((res) => res.data.data)
+    .catch((e) => e);
 }
